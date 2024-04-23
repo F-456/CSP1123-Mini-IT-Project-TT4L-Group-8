3d64a4cbb76f35d52502c22e8fee3199c6ac6758
ZH Low <fzhao989898@gmail.com>
2024-04-23 15:11:25 +0800

    Sound control

--- Main.py
+++ Main.py
@@ -7,11 +7,6 @@ pygame.mixer.init()
 clock=pygame.time.Clock()
 fps = 60
 
-# add background music
-BGM=pygame.mixer.Sound('Sound/BGM.mp3')
-BGM.set_volume(1.5)
-BGM.play()
-
 #differentiate different size for game window 
 screen_width=800
 screen_height=800
@@ -47,16 +42,52 @@ class Display():
         rect_colour = grey
         pygame.draw.rect(screen,(rect_colour),Middle_rect)
 
+# add background music
+pygame.mixer.music.load('Sound/BGM.mp3')
+pygame.mixer.music.set_volume(0.5)
+pygame.mixer.music.play(-1)
+
+# Define button parameters
+button_width = 200
+button_height = 50
+button_x = (screen_width - button_width) // 2
+button_y = screen_height - 100
+button_color = (0, 255, 0)
+button_text_color = (255, 255, 255)
+button_font = pygame.font.Font(None, 36)
+button_text = "Pause Music"
+music_paused = False
+
 #main run for game
 run = True
 while run:
     clock.tick(fps)
+    screen.fill((0,0,0))
+    screen.blit(background,(0,0))
+
     Display.middle()
     Display.drawing_grid(100)
+
+    # Draw the music control button
+    pygame.draw.rect(screen, button_color, (button_x, button_y, button_width, button_height))
+    button_surface = button_font.render(button_text, True, button_text_color)
+    screen.blit(button_surface, (button_x + 20, button_y + 10))
+
       
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
-            run = False  
+            run = False 
+        elif event.type == pygame.MOUSEBUTTONDOWN:
+            mouse_x, mouse_y = pygame.mouse.get_pos()
+            if button_x <= mouse_x <= button_x + button_width and button_y <= mouse_y <= button_y + button_height:
+                if music_paused:
+                    pygame.mixer.music.unpause()
+                    button_text = "Pause Music"
+                    music_paused = False
+                else:
+                    pygame.mixer.music.pause()
+                    button_text = "Play Music"
+                    music_paused = True
 
     pygame.display.update()
 pygame.quit()
